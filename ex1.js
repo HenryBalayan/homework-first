2fbd80142e69b9fcbf84ce4c01e6e923d27fb193
HenryBalayan <58569241+HenryBalayan@users.noreply.github.com>
2019-12-13 14:00:05 +0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,141 @@
+
+/*ex1*/
+/*
+  let number = 120;
+  number = number%2==0?"even":"odd";
+  console.log(number);
+ */
+
+/*ex2*/
+/*
+ let a = +prompt();
+ let b = +prompt();
+ if(a%b==0||b%a==0){
+
+      alert("1");
+
+ }
+ else{
+ 	  alert("0");
+ }
+
+ */
+/*ex3*/
+/*
+let num= +prompt();
+ let a = num;
+ let b=a+a;
+ let c=b+a;
+ console.log(a+""+b +""+c);
+
+*/
+/*ex4*/
+/*
+let num =+prompt();
+
+if(num%10!=0){
+let a= num%10;
+let b =a+""+num;
+let c=b/10;
+console.log(Math.floor(c));
+}
+else{
+
+	console.log(num);
+}
+
+
+*/
+/*ex5*/
+/*
+let num1 =+prompt();
+let num2 =+prompt();
+let num3 =+prompt();
+let num4 =+prompt();
+let num5 =+prompt();
+let total=num1+num2+num3+num4+num5;
+console.log(total/5);
+*/
+
+
+/*ex6*/
+/*
+let num1 = +prompt();
+let num2 = +prompt();
+let num3 = +prompt();
+if(num1<=num2 && num2<=num3){
+	console.log(num1,num2,num3);
+}
+
+else if(num1<=num3 && num3<=num2){
+	console.log(num1,num3,num2);
+}
+
+ else if(num2<=num1 && num1<=num3){
+	console.log(num2,num1,num3);
+}
+
+ else if(num2<=num3 && num3<=num1){
+	console.log(num2,num3,num1);
+}
+
+ else if(num3<=num1 && num1<=num2){
+
+	console.log(num3,num1,num2);
+}
+
+
+else {
+	 console.log(num3,num2,num1);
+}
+
+
+*/
+
+           /*ex7*/
+
+           /*
+             let a = +prompt();
+             let b = +prompt();
+             let c = +prompt();
+         if (a > 0 && b > 0 && c > 0)
+            {
+                console.log("+");
+            }
+            else if (a < 0 && b < 0 && c < 0)
+            {
+                console.log("-");
+            }
+            else if (a > 0 && b > 0 && c < 0)
+            {
+              console.log("-");
+            }
+            else if (a > 0 && b < 0 && c > 0)
+            {
+              console.log("-");
+            }
+            else if (a < 0 && b > 0 && c > 0)
+            {
+              console.log("-");
+            }
+            else if (a > 0 && b < 0 && c < 0)
+            {
+              console.log("+");
+            }
+            else if (a < 0 && b < 0 && c > 0)
+            {
+                console.log("+");
+            }
+            else if (a < 0 && b > 0 && c < 0)
+            {
+                 console.log("+");
+            }
+       else
+            {
+              console.log("unsigned");
+            }
+            */
+
+
+
+     
